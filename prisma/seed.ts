551f0e1f0d4699a3b2cea6b4f26f6f307f94b7b9
Karla Duarte Ferreira <karladuarte87@hotmail.com>
2025-12-29 13:37:17 -0300

    Update image URL in seed data to enhance visual quality

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -37,7 +37,7 @@ async function main() {
       author: 'Karla Duarte Ferreira',
       publish_date: '27 de Dezembro de 2024',
       category: 'Tecnologia',
-      image_url: 'https://picsum.photos/seed/tech1/800/450',
+      image_url: 'https://images.unsplash.com/photo-1746286720984-72f386e1872e?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     },
   });
 
